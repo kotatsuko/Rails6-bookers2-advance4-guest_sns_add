4b0e56b3b3bc205e76cd1af3636d14981ad33cb0
kotatsuko <107920100+kotatsuko@users.noreply.github.com>
2022-07-13 01:47:28 +0000

    [add]投稿一覧いいね順（一週間）

---
+++ .bash_profile
@@ -0,0 +1,6 @@
+export LD_LIBRARY_PATH="/opt/sqlite/sqlite3/lib"
+export LD_LIBRARY_PATH="/opt/sqlite/sqlite3/lib"
+export LD_LIBRARY_PATH="/opt/sqlite/sqlite3/lib"
+export LD_LIBRARY_PATH="/opt/sqlite/sqlite3/lib"
+export LD_LIBRARY_PATH="/opt/sqlite/sqlite3/lib"
+export LD_LIBRARY_PATH="/opt/sqlite/sqlite3/lib"
---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "homes#top"
+  get "home/about"=>"homes#about"
+  get "search" => "searches#search"
+  devise_for :users
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  get "books/favorited/week" => "books#index_favorited_week" ,as: "books_favorited_week"
+  resources :users, only: [:index,:show,:edit,:update]do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+end
